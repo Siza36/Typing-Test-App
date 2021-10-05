2d8d295fc6de6a82b872de031c1aae2146b48f5a
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2021-10-05 18:48:07 +0330

    Random SENTENCEs aded

--- Javascript.js
+++ Javascript.js
@@ -2,12 +2,42 @@
 let timer = document.querySelector("#time");
 let sec = 0;
 let min = 0;
-setInterval(function(){
-sec++;
-if(sec >= 60){
-min++;
-sec=0;
-}
-timer.innerText = `${min<10?"0":""}${min}:${sec<10?"0":""}${sec}`;
-},1000)
 
+//handle the timer
+const ipt = document.querySelector("#maininout");
+ipt.addEventListener("focus",function timerhandl(){
+    setInterval(function(){
+        sec++;
+        if(sec >= 60){
+        min++;
+        sec=0;
+        }
+        timer.innerText = `${min<10?"0":""}${min}:${sec<10?"0":""}${sec}`;
+        },1000)
+})
+//random text
+const randomText = [
+    "A small town lies between the big cities."
+    ,"Don't go there and don't stay here!",
+    "You and Iron Man are two great men."
+    ,"Is this big enough for you?"
+    ,"Your face looks like a big rock!"
+    ,"Is Tehran a big city in Iran?"
+    ,"You’re a fantastic programer."
+    ,"Don't open the hatch it's rainy!"
+    ,"You are the Weakest Link!"
+    ,"You can't miss it so don't."
+    ,"You should wash the dishes."
+    ,"If you go there, you will find him."
+    ,"It’s fascinating that you know me."];
+    const randomSentences =randomText[Math.floor( Math.random()*12)];
+    const displayRandom = document.querySelector("#txt").innerText=randomSentens;
+    console.log(randomSentens);
+//submit the text 
+const sub = document.querySelector("#btn");
+sub.addEventListener("click",function sub(){
+    if(ipt.value === randomSentens){
+        alert(`well done!!!! ${timer.innerText}`);
+        ipt.value ="";
+    }
+});
